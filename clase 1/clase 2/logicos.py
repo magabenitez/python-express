93930a436ef2aa38138a7f5c9f277ce5b677c88c
Magali Benitez Vazquez <magalibenitez004@gmail.com>
2025-07-05 07:23:55 -0300

    logicos ya finalizado

---
+++ clase 1/clase 2/logicos.py
@@ -0,0 +1,20 @@
+'''
+or
+and
+not
+'''
+
+edad= 22
+if (edad>=0 or edad<=18):
+    print('es menor de edad')
+else:
+    print('es mayor de edad')
+#Busque otra manera ya que al ser mayor de edad no se cumple la condición
+#Todos los numeros arrojaban que era menor de edad
+
+
+edad= 22
+if 0<= edad <= 18:
+    print('es menor de edad')   
+else:
+    print('es mayor de edad')
