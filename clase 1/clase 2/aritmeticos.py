4aa8fdc074cad5941482bb01c7d9d787c58aa39f
Magali Benitez Vazquez <magalibenitez004@gmail.com>
2025-07-05 06:56:36 -0300

    aritmeticos ya finalizado

---
+++ clase 1/clase 2/aritmeticos.py
@@ -0,0 +1,41 @@
+'''
+Operadores aritméticos basicos en Python
+suma,resta,multiplicación y division de dos números
+c=a+b
+d=d-a
+e=a*b
+f=a/b
+g=a%b
+h=a**b
+'''
+a = 10
+b = 5
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+h = a ** b
+print("Suma:", c)
+print("Resta:", d)
+print("Multiplicación:", e)
+print("División:", f)
+print("Módulo:", g)
+print("Exponente:", h)
+# Segunda forma
+
+a= 10
+b= 5
+r= 0
+r= a + b
+print("Suma:", r)
+r= a - b
+print("Resta:", r)
+r= a * b
+print("Multiplicación:", r)
+r= a / b
+print("División:", r)
+r= a % b
+print("Módulo:", r)
+r= a ** b
+print("Exponente:", r)
