73d6da4ce8cae6c9c506fa691018bd595b543e7e
Magali Benitez Vazquez <magalibenitez004@gmail.com>
2025-07-05 04:58:07 -0300

    variables ya finalizado

---
+++ clase 1/clase 2/variables.py
@@ -0,0 +1,31 @@
+'''
+Comentario de varias lineas
+Proident consequat nisi amet occaecat proident in do et aliqua ipsum est. Reprehenderit excepteur fugiat minim mollit ullamco excepteur aliquip magna velit nulla. Est culpa adipisicing excepteur nostrud exercitation. Sunt ipsum nulla sit exercitation nisi laborum tempor enim. Elit consequat ad magna tempor voluptate dolor aliquip est ea dolor dolore enim voluptate non.
+
+Enim magna sit nostrud cillum aute reprehenderit elit aliquip minim aliqua labore. Veniam duis esse cillum laboris consequat duis quis ullamco magna voluptate incididunt irure reprehenderit magna. Mollit eu anim adipisicing elit proident do consectetur nisi culpa labore et quis id. Tempor cillum voluptate voluptate eiusmod aliquip irure exercitation est nulla irure. Magna nisi reprehenderit magna do consequat est Lorem ipsum elit. Ullamco fugiat anim tempor aliqua sunt duis do proident Lorem tempor aute minim voluptate mollit.
+
+Excepteur enim sit culpa ullamco. Voluptate reprehenderit est tempor quis do consectetur labore veniam commodo ea id amet proident. Magna non aliqua ad minim dolore aliqua. Nisi ad eiusmod ipsum dolore reprehenderit labore. Duis duis incididunt exercitation reprehenderit cupidatat consequat exercitation exercitation cupidatat.
+
+Mollit minim proident consectetur ad sunt mollit nisi eu. Veniam excepteur in culpa tempor. Nostrud laboris eiusmod sunt dolor occaecat sint elit dolor labore laboris aliqua. Ex mollit laborum eu do nostrud. Dolor amet culpa deserunt nostrud aute eu. Sunt velit adipisicing cillum ad fugiat nulla dolore commodo dolor consequat et.
+'''
+
+print("Hola mundo desde casa") 
+entero=10
+flotante=10.5
+cadena="Hola mundo"
+booleano=True
+print(entero, flotante, cadena,booleano)
+print(type(entero),type(flotante),type(cadena),type(booleano))
+#Operaciones basicas
+#Suma, resta, multiplicacion, division
+print("Suma de dos numeros")
+a=20
+b=30
+print("suma de dos numeros",a+b)
+print("Resta de dos numeros")
+print("resta de dos numeros",a-b)
+print("multiplicacion de dos numeros")
+print("multiplicacion de dos numeros",a*b)
+b=4
+print("Division de dos numeros")        
+print("Division de dos numeros",a/b)
