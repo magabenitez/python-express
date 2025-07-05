905d18f88e433c2f4ce9f5a32e2b80df6f182439
Magali Benitez Vazquez <magalibenitez004@gmail.com>
2025-07-05 05:26:39 -0300

    operadores ya finalizado

---
+++ clase 1/clase 2/operadores.py
@@ -0,0 +1,25 @@
+'''
+operadores de comparación   
+a == b
+a != b
+a > b
+a < b
+a >= b
+a <= b
+'''
+
+
+a= 17 #Se solicita al usuario que ingrese un valor
+b= 18 
+r= a == b
+print(r)
+r= a != b
+print(r)
+r= a > b
+print("Es menor",r)
+r= a < b
+print("Es mayor",r)
+r= a >= b
+print(r)
+r= a <= b
+print(r)
